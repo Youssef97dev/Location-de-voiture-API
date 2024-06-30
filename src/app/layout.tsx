89bd24340729c855f219c6726e1f8d440237ac09
Youssef97dev <youssefmoussafi97@gmail.com>
2024-06-30 07:11:40 -1100

    Create the Navbar and change the global style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 import { AuthProvider } from "./Providers";
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-          <div>{children}</div>
+          <div>
+            <Navbar />
+            {children}
+          </div>
         </AuthProvider>
       </body>
     </html>
